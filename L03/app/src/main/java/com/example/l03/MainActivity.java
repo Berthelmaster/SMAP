41dfc7cf8a5fc4bfffaab16c865e5411bc6733e7
Berthelmaster <Berthel0802@gmail.com>
2020-02-13 16:17:39 +0100

    L03 partly finished

--- L03/app/src/main/java/com/example/l03/MainActivity.java
+++ L03/app/src/main/java/com/example/l03/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.l03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button EditText;
+    Button Picker;
+    Button Sliders;
+    int number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText = findViewById(R.id.EditTextButton);
+        Picker = findViewById(R.id.PickerButton);
+        Sliders = findViewById(R.id.SlidersButton);
+
+        EditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent EditTextIntent = new Intent(MainActivity.this, EditTextActivity.class);
+                startActivity(EditTextIntent);
+            }
+        });
+        Picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent EditTextIntent = new Intent(MainActivity.this, PickerActivity.class);
+                startActivity(EditTextIntent);
+            }
+        });
+        Sliders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent EditTextIntent = new Intent(MainActivity.this, SlidersActivity.class);
+                startActivity(EditTextIntent);
+            }
+        });
     }
 }
